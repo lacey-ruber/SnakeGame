61e0b52ef29e9fe021b7b81099127929ff1fd9f8
Veronika Kuznetsova <lassie.ruber@gmail.com>
2023-09-19 15:20:44 +0700

    Base functionality

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -8,7 +8,7 @@ public class MainWindow extends JFrame {
     setTitle("Змейка");
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     setSize(320, 345);
-    setLocation(400,400);
+    setLocation(400, 400);
     add(new GameField());
     setVisible(true);
   }
